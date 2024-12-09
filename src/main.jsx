af38bfd56ea3e20da903c72fbbfda5f22f958314
olaide <holaryidealabi@gmail.comgit config --global user.email holaryidealabi@gmail.com>
2024-12-09 11:01:26 -0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ToggleProvider } from "./contexts/toggleContext";
+import { ToggleProvider } from "./contexts/ToggleContext";
 import { LoginProvider } from "./contexts/LoginContext";
 
 createRoot(document.getElementById("root")).render(
